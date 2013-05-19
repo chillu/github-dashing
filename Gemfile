7cf1fa7605a80c9f1ed0d4e302c950e963e8396c
Ingo Schommer <ingo@silverstripe.com>
2013-05-19 18:31:23 +0200

    Pull requests widget (and default dashboard)

--- Gemfile
+++ Gemfile
@@ -1,3 +1,6 @@
 source 'https://rubygems.org'
 
 gem 'dashing'
+gem 'google-api-client'
+gem 'json', '~> 1.7.7'
+# gem 'yaml'
---
+++ lib/big_query_backend.rb
@@ -0,0 +1,78 @@
+require 'google/api_client'
+
+# Interacts with the Google BigQuery API. 
+# Handles authentication and API discovery.
+class BigQueryBackend
+
+	attr_accessor :client, :keyfile, :secret, :issuer, :project_id
+	attr_reader :api, :is_authenticated
+
+	def initialize(args={})
+		@client = Google::APIClient.new(
+			:application_name => 'Github Dashing BigqueryBackend',
+			:application_version => '0.1'
+		)
+		# @client.logger.level = Logger::DEBUG
+		
+		args.each do |k,v|
+      instance_variable_set("@#{k}", v) unless v.nil?
+    end
+	end
+
+	def query(query)
+		self.authenticate
+		
+		result = @client.execute(
+		  :api_method => @api.jobs.query,
+		  :parameters => {'projectId' => @project_id},
+		  :body_object => {'query' => query}
+		)
+		# TODO Check for query success (or retry)
+
+		return result
+	end
+
+	def authenticate()
+		if not @is_authenticated
+			@api = @client.discovered_api('bigquery', "v2")
+			@client.authorization = Signet::OAuth2::Client.new(
+			  :token_credential_uri => 'https://accounts.google.com/o/oauth2/token',
+			  :audience => 'https://accounts.google.com/o/oauth2/token',
+			  :scope => 'https://www.googleapis.com/auth/bigquery',
+			  :issuer => @issuer,
+			  :signing_key => Google::APIClient::KeyUtils.load_from_pkcs12(File.open(@keyfile), @secret)
+			)
+			@client.authorization.fetch_access_token!
+			# TODO Check for auth success
+			@is_authenticated = true
+		end
+	end
+
+	def pull_requests_by_period(period='month', owners=[], repos=[])
+		case period
+		when 'day'
+			period_filter = 10
+		when 'month'
+			period_filter = 7
+		when 'year'
+			period_filter = 4
+		end
+		owners_filter = owners.map{|owner|"repository_owner='#{owner}'"}.join(' OR ')
+		repos_filter = repos.map{|repo|"repository_name='#{repo}'"}.join(' OR ')
+		query = <<eos
+			SELECT LEFT(created_at, #{period_filter}) as period, COUNT(*) as total_count
+			FROM [githubarchive:github.timeline]
+			WHERE
+			    type='PullRequestEvent' 
+			    AND payload_action = 'opened'
+			    AND (#{owners_filter})
+			    AND (#{repos_filter})
+			GROUP BY period
+			ORDER BY period ASC;
+eos
+
+		client.logger.debug(query)
+		
+		return self.query(query)
+	end
+end
