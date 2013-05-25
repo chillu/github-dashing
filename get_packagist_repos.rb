1b96aa73f7b8c0ea2d89b67f76d5505e141d2a66
Ingo Schommer <ingo@silverstripe.com>
2013-05-26 00:34:00 +0200

    Packagist repos helper script

---
+++ get_packagist_repos.rb
@@ -0,0 +1,27 @@
+# Collect all modules of a specific type from packagist.org
+# Useful for configuring the REPOS configuration key
+
+require 'json'
+require 'net/https'
+require 'cgi'
+
+type='silverstripe-module'
+results = []
+nexturl = "https://packagist.org/search.json?type=#{type}"
+i = 0
+while nexturl and i < 100 do
+	# puts "Loading #{nexturl}"
+	uri_obj = URI.parse(nexturl)
+	break unless uri_obj and uri_obj.request_uri
+	http = Net::HTTP.new(uri_obj.host, uri_obj.port)
+	http.use_ssl = true
+	http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+	request = Net::HTTP::Get.new(uri_obj.request_uri)
+	response = http.request(request)
+	part = JSON.parse(response.body)
+	results += part['results']
+	i += 1
+	nexturl = part['next'] ? CGI::unescape(part['next'].to_s).sub('[0]','') : nil
+end
+
+puts results.collect {|result|result['name']}.join(',')
