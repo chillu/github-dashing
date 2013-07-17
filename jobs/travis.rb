8f6fb0179404afbaba38185bfcfb92da7669de08
Ingo Schommer <ingo@silverstripe.com>
2013-07-17 17:10:39 +0200

    Hide travis branches if all passed, fix overall build status class, branch whitelist to avoid feature branches showing

--- jobs/travis.rb
+++ jobs/travis.rb
@@ -9,6 +9,7 @@ SCHEDULER.every '1h', :first_in => '1s' do |job|
 	backend = TravisBackend.new
 	repo_slugs = []
 	builds = []
+	branch_whitelist = /^(\d+\.\d+|master)/
 
 	if ENV['ORGAS']
 		ENV['ORGAS'].split(',').each do |orga|
@@ -25,17 +26,22 @@ SCHEDULER.every '1h', :first_in => '1s' do |job|
 	items = repo_slugs.map do |repo_slug|
 		repo_builds = backend.get_builds_by_repo(repo_slug)
 		# Get the newest build for each branch
-		branches = repo_builds.group_by {|build|build['branch']}.map do |branch,builds|
-			{
-				'class'=>(builds[0]['result'] == 0) ? 'good' : 'bad', # POSIX return code
-				'label'=>builds[0]['branch'],
-				'title'=>builds[0]['finished_at'],
-				'url'=> 'https://travis-ci.org/%s/builds/%d' % [repo_slug,builds[0]['id']]
-			}
-		end
+		branches = repo_builds
+			.group_by {|build|build['branch']}
+			.select {|branch,builds_for_branch|branch_whitelist.match(branch) }
+			.map do |branch,builds_for_branch|
+				{
+					'class'=>(builds_for_branch[0]['result'] == 0) ? 'good' : 'bad', # POSIX return code
+					'label'=>builds_for_branch[0]['branch'],
+					'title'=>builds_for_branch[0]['finished_at'],
+					'result'=>builds_for_branch[0]['result'],
+					'url'=> 'https://travis-ci.org/%s/builds/%d' % [repo_slug,builds_for_branch[0]['id']]
+				} 
+			end
+
 		{
 			'label'=>repo_slug,
-			'class'=> (branches.min_by{|b|b['result']} == 0) ? 'good' : 'bad', # POSIX return code
+			'class'=> (branches.collect{|b|b['result']}.max == 0) ? 'good' : 'bad', # POSIX return code
 			'url' => 'https://travis-ci.org/%s' % repo_slug,
 			'items' => branches
 		}
