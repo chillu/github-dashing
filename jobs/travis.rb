e9cc81b629d4f13299f31142c5311a5ba14576a5
Ingo Schommer <ingo@silverstripe.com>
2013-07-17 19:20:22 +0200

    Allow for unknown travis builds

--- jobs/travis.rb
+++ jobs/travis.rb
@@ -25,26 +25,35 @@ SCHEDULER.every '1h', :first_in => '1s' do |job|
 
 	items = repo_slugs.map do |repo_slug|
 		repo_builds = backend.get_builds_by_repo(repo_slug)
-		# Get the newest build for each branch
-		branches = repo_builds
-			.group_by {|build|build['branch']}
-			.select {|branch,builds_for_branch|branch_whitelist.match(branch) }
-			.map do |branch,builds_for_branch|
-				{
-					'class'=>(builds_for_branch[0]['result'] == 0) ? 'good' : 'bad', # POSIX return code
-					'label'=>builds_for_branch[0]['branch'],
-					'title'=>builds_for_branch[0]['finished_at'],
-					'result'=>builds_for_branch[0]['result'],
-					'url'=> 'https://travis-ci.org/%s/builds/%d' % [repo_slug,builds_for_branch[0]['id']]
-				} 
-			end
-
-		{
-			'label'=>repo_slug,
-			'class'=> (branches.collect{|b|b['result']}.max == 0) ? 'good' : 'bad', # POSIX return code
-			'url' => 'https://travis-ci.org/%s' % repo_slug,
-			'items' => branches
-		}
+		if repo_builds and repo_builds.length > 0
+			puts repo_slug
+			# Get the newest build for each branch
+			branches = repo_builds
+				.group_by {|build|build['branch']}
+				.select {|branch,builds_for_branch|branch_whitelist.match(branch) }
+				.map do |branch,builds_for_branch|
+					{
+						'class'=>(builds_for_branch[0]['result'] == 0) ? 'good' : 'bad', # POSIX return code
+						'label'=>builds_for_branch[0]['branch'],
+						'title'=>builds_for_branch[0]['finished_at'],
+						'result'=>builds_for_branch[0]['result'],
+						'url'=> 'https://travis-ci.org/%s/builds/%d' % [repo_slug,builds_for_branch[0]['id']]
+					} 
+				end
+			{
+				'label'=>repo_slug,
+				'class'=> (branches.reject{|b|b['result'] != nil and b['result'] == 0}.count > 0) ? 'bad' : 'good', # POSIX return code
+				'url' => branches.count ? 'https://travis-ci.org/%s' % repo_slug : '',
+				'items' => branches
+			}
+		else
+			{
+				'label'=>repo_slug,
+				'class'=> 'none',
+				'url' => '',
+				'items' => []
+			}
+		end
 	end
 	
 	send_event('travis', {
