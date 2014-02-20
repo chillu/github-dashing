4c8c5d9a33755535090081cb9873936f662b20b5
Ingo Schommer <ingo@silverstripe.com>
2014-02-20 22:25:24 +1300

    Filter out pull requests from issue stats (fixes #10)

--- lib/github_backend.rb
+++ lib/github_backend.rb
@@ -167,7 +167,9 @@ class GithubBackend
 			['open','closed'].each do |state|
 				begin
 					issues = @client.issues(repo, {:since => opts.since,:state => state})
-					issues = issues.select {|issue|issue.created_at.to_datetime > opts.since.to_datetime}
+					issues = issues.select do |issue|
+						issue.created_at.to_datetime > opts.since.to_datetime and not issue.pull_request.html_url
+					end
 					state_desc = (state == 'open') ? 'opened' : 'closed'
 					issues.each do |issue|
 						events << GithubDashing::Event.new({
