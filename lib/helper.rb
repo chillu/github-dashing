e42b95b44f8ec0531e021a2d22e58f66ddb18b40
Ingo Schommer <ingo@silverstripe.com>
2014-07-03 10:41:31 +1200

    Don't use sign and arrow to indicate trend

--- lib/helper.rb
+++ lib/helper.rb
@@ -1,10 +1,10 @@
 module GithubDashing
 	class Helper
 
-		def self.trend_percentage(last, current)
+		def self.trend_percentage(last, current, use_sign=false)
 			if last.to_f > 0 # avoid division by infinity
 				trend = (current.to_f / last.to_f)*100-100
-				sign = trend > 0 ? '+' : ''
+				sign = (trend > 0 and use_sign) ? '+' : ''
 			else 
 				trend = 0
 				sign = ''
@@ -15,10 +15,10 @@ module GithubDashing
 		# Compare to last month by using multiplier based on month "progress"
 		# Example: 50 requests in all of March, 40 requests until 15th of April.
 		# Multiplier is 0.5 (month halfway through) -> (40/(50*0.5)*100)-100 -> 60% increase
-		def self.trend_percentage_by_month(last, current)
+		def self.trend_percentage_by_month(last, current, use_sign=false)
 			if last.to_f > 0 # avoid division by infinity
 				trend = (current.to_f / last.to_f * (31/Time.now.day.to_f))*100-100
-				sign = trend > 0 ? '+' : ''
+				sign = (trend > 0 and use_sign) ? '+' : ''
 			else 
 				trend = 0
 				sign = ''
