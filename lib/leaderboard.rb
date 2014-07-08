763d39598a7697ddafb3f57bbcbddbd5d1147483
Jess Portnoy <jess.portnoy@kaltura.com>
2014-07-08 10:08:19 -0400

    ingore users belonging to org $SKIP_ORG_MEMBERS. This is useful if you are a company with an open project and would like to only view outside contributions

--- lib/leaderboard.rb
+++ lib/leaderboard.rb
@@ -31,7 +31,6 @@ class Leaderboard
 			@backend.issue_count_by_author(opts).to_a +
 			@backend.pull_count_by_author(opts).to_a
 		)
-
 		# TODO Pretty much everything below would be better expressed in 
 		# SQL with 1/4th the lines of code
 
@@ -51,6 +50,7 @@ class Leaderboard
 		# Add score for each period
 		actors_scored = {}
 		events_by_actor.each do |actor,actor_data|
+			next if is_from_org(ENV['SKIP_ORG_MEMBERS'],actor) === true
 			actor_data['periods'].each do |period,period_data|
 				desc = []
 				period_data['score'] = period_data.inject(0) do |c,(k,v)|
@@ -87,3 +87,14 @@ class Leaderboard
 
 
 end
+	def is_from_org(org,user)
+		client = Octokit::Client.new(
+			:login => ENV['GITHUB_LOGIN'],
+			:access_token => ENV['GITHUB_OAUTH_TOKEN']
+		)
+		result = client.organization_member?(org, user)
+		client = nil
+		GC.start
+		Octokit.reset!
+		return result
+	end
